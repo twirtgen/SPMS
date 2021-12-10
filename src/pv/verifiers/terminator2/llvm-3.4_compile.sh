68a8ce4da3c9686131216c1f50cf7ed9df124b18
nrybowski <nicolas.rybowski@uclouvain.be>
2021-12-11 00:21:01 +0100

    verifiers:t2: Add correct compiler flags

--- src/pv/verifiers/terminator2/llvm-3.4_compile.sh
+++ src/pv/verifiers/terminator2/llvm-3.4_compile.sh
@@ -1,4 +1,4 @@
 #! /bin/bash -xe
 
 L=($(echo "${1}" | sed $'s/\// /g'))
-clang -I/mount/picoquic -Wall -Wextra -c -emit-llvm -O0 /mount/${1}.c -o "/output/${L[-1]}.bc"
+clang -DPROVERS -DPROVERS_T2 -Wall -Wextra -c -emit-llvm -O0 /mount/${1}.c -o "/output/${L[-1]}.bc"
