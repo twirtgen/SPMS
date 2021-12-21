69d89399d4c2f77918fb3b142a428fb610dc12e1
twirtgen <thomas.wirtgen@uclouvain.be>
2021-12-21 18:05:17 +0100

    Rewrite wrongly generated kittel file

--- src/pv/verifiers/terminator2/llvm2kittel.sh
+++ src/pv/verifiers/terminator2/llvm2kittel.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
 L=($(echo "${1}" | sed $'s/\// /g'))
-./llvm2kittel --eager-inline -increase-strength -no-slicing --t2 --function "${L[-1]}" /mount/${1}.bc > /mount/${1}.t2
+./rewrite_kittel.py < $(./llvm2kittel --eager-inline -increase-strength -no-slicing --t2 --function "${L[-1]}" /mount/${1}.bc) > /mount/${1}.t2
+
